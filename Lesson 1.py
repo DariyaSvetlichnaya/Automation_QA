cdfb0def595c915f04e754d784d1641c0867357a
DariyaSvetlichnaya <d.a.svetlichnaya@gmail.com>
2024-01-15 22:16:53 +0100

    Homework for the Lesson 1

---
+++ Lesson 1.py
@@ -0,0 +1,39 @@
+# Create variables john_salary and marta_salary of some type (floating point)
+john_salary = 1.5
+marta_salary = 2.6
+
+print(john_salary)
+print(marta_salary)
+
+# Create variables john_age and marta_age of integer type
+john_age = 35
+marta_age = 30
+
+print(john_age)
+print(marta_age)
+
+# Create string type variables john_name and marta_name
+john_name = 'John'
+marta_name = 'Marta'
+
+print(john_name)
+print(marta_name)
+
+# Create boolean variables john_gender and marta_gender. Let john be false and Marta be true.
+john_gender = False
+marta_gender = True
+
+print(john_gender)
+print(marta_gender)
+
+# Create variables john_friends and marta_friends. Assign lists to variables
+john_friends = ['Gabriel', 'Mark', 'Vicky']
+marta_friends = ['Victoria', 'Lorena', 'John']
+
+print(john_friends)
+print(marta_friends)
+
+# Create a list with people's names, some names should be repeated in it
+people = {'Austin', 'Gabriel', 'Gregory', 'Austin'}
+
+print(people)
