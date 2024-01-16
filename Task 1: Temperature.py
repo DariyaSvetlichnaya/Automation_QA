da958bddf1b3bea1a19f6d4473c27c9bbf73afe5
DariyaSvetlichnaya <d.a.svetlichnaya@gmail.com>
2024-01-16 22:35:18 +0100

    Lesson 1/Task 1

---
+++ Task 1: Temperature.py
@@ -0,0 +1,12 @@
+# User's input in Celsius
+celsius = float(input('Please enter temperature in Celsius: '))
+
+# Converting to Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Converting to Kelvin
+kelvin = celsius + 273.15
+
+# Printing the results in Fahrenheit and Kelvin
+print('%0.1f Celsius = %0.1f Fahrenheit.' % (celsius, fahrenheit))
+print('%0.1f Celsius = %0.1f Kelvin.' % (celsius, kelvin))
