ddfe1947ca815b14ef6b359b78680109444b9ed0
DariyaSvetlichnaya <d.a.svetlichnaya@gmail.com>
2024-01-18 15:41:29 +0100

    Lesson 1/Task 2

---
+++ Calculator.py
@@ -0,0 +1,35 @@
+# Prompt to input numbers
+number_1 = int(input('Enter the first number: '))
+number_2 = int(input('Enter the second number: '))
+
+#Select the operator
+operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
+
+#Counting
+if operation == '+':
+    print('{} + {} = '.format(number_1, number_2))
+    print(number_1 + number_2)
+
+elif operation == '-':
+    print('{} - {} = '.format(number_1, number_2))
+    print(number_1 - number_2)
+
+elif operation == '*':
+    print('{} * {} = '.format(number_1, number_2))
+    print(number_1 * number_2)
+
+elif operation == '/':
+    if number_2 != 0:
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+    else:
+        print("Cannot divide by 0")
+
+else:
+    print('You have not typed a valid operator, please run the program again.')
