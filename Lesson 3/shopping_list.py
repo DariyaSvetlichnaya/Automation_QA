7884a8a6125a0e18a3851a32f753cd68b2f59ecc
DariyaSvetlichnaya <d.a.svetlichnaya@gmail.com>
2024-01-23 00:03:42 +0100

    Shopping list 4st commit

--- Lesson 3/shopping_list.py
+++ Lesson 3/shopping_list.py
@@ -2,26 +2,24 @@ def create_shopping_list():
     # Step 1: Prompt the user to create a list
     user_input = input("\n"
                        "You have a unique chance to create your own shopping list!\n"
-                       "Please, enter the products separated by spaces: \n"
-                       "    ")
+                       "Please, enter the products separated by spaces: \n")
 
     # Step 2: Convert the string entered by the user to the list
     shopping_list = user_input.split()
 
     # Step 3: Check if the user entered products and print the list
     if any(shopping_list):
-        print(f"Here is your shopping list: {shopping_list}")
+        print(f"\nHere is your shopping list: {shopping_list}")
     else:
         print("Unfortunately, your shopping list is empty :( See you next time!")
         return
 
     # Step 5: Prompt the user to enter the product
     while True:
-        action_input = input('''
-        To ADD the product to the list: just enter the product name (ex., 'milk')
-        To REMOVE the product from the list: enter '-' before the product's name (ex., '-banana'): ''').strip()
+        action_input = input("\nTo ADD the product to the list: just enter the product name (ex., milk)\n"
+                             "To REMOVE the product from the list: enter \'-\' before the name (ex., -milk):\n").strip()
 
-    # Step 6: Add/remove product
+        # Step 6: Add/remove product
         if action_input.startswith('-'):
             # Remove product:
             product_to_remove = action_input[1:]
@@ -33,14 +31,14 @@ def create_shopping_list():
         else:
             # Add product
             shopping_list.append(action_input)
-            print(f"{action_input} is added to the shopping list.")
+            print(f"\n{action_input} is added to the shopping list.")
 
         # Step 7: Print the updated list of products
-        print(f"Here is the updated shopping list: {shopping_list}")
+        print(f"\nHere is the updated shopping list: {shopping_list}")
 
         # Step 8: Check if the list is empty using len
         if len(shopping_list) == 0:
-            print("The shopping list is empty. That's all for today!")
+            print("\nThe shopping list is empty. That's all for today!")
             break
 
 
