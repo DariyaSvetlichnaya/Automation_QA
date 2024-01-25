0a53347705fa9116a31788cb49c7bf0f48f42f9e
DariyaSvetlichnaya <d.a.svetlichnaya@gmail.com>
2024-01-25 22:56:44 +0100

    last commit

---
+++ lesson 4/home.py
@@ -0,0 +1,7 @@
+# Create dictionary
+home = {'Alex': 'house', 'Max': 'Flat', 'Olha': 'Appartments', 'Oleh': 'Trench'}
+
+# Printing where every person is living
+for k, v in home.items():
+    print(f"{k} is living in {v}")
+
