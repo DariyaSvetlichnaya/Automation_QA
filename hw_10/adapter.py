cc80c7c789c2f59e5aec3689e14479b6a3a612d7
DariyaSvetlichnaya <d.a.svetlichnaya@gmail.com>
2024-03-01 23:47:21 +0100

    change naming

---
+++ hw_10/adapter.py
@@ -0,0 +1,21 @@
+class TextAdapter:
+
+    @staticmethod
+    def csv_to_html(file_path):
+        with open(file_path) as f:
+            res = f.readlines()
+
+            header = [k.strip() for k in res[0].split(',')]
+
+            rows = res[1:]
+
+            html_output = ''
+
+            for row in rows:
+                row = row.strip().split(',')
+                html_output += ''.join(f"<{h}>{r}</{h}>\n" for h, r in zip(header, row))
+
+        return html_output
+
+
+print(TextAdapter.csv_to_html('team_salary.txt'))
